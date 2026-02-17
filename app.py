fcd1d260beda081ea293441cd180d2a5cc828e1b
microdegree <microdegree@gmail>
2026-02-17 10:27:31 +0530

    Adding app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+print ("Hello DevOps! \nThis app is running inside a Docker Container, Managed by jenkins.")
+
+
+#import datetime
+
+#now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#print(f"[{now}] Hello DevOps!\n[{now}] This app is running inside Docker.")
