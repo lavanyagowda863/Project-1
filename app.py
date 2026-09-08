21a5fed00967cc1aad05f0d7d69469bfa925dee2
lavanyagowda863 <lavanyagowda863@gmail.com>
2026-09-08 20:03:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-print ("Hello DevOps class! \nThis app is running inside a Docker Container")
+print ("Hello DevOps class! \nThis app is running inside a Docker Container, \ntesting here")
 
 
 #import datetime
